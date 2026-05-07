db6f47be30a3c52ac041f6fb93af3a2e6f786a0b
trdyer06 <trdyer06@gmail.com>
2026-05-06 21:48:28 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+const question = {
+    question: 'Who painted the Mona Lisa?',
+    answers: [
+        'Leonardo Da Vinci',
+        'Michelangelo',
+        'Raphael',
+        'Donatello'
+    ],
+    correctAnswer: 'Leonardo Da Vinci',
+    points: 2
+};
